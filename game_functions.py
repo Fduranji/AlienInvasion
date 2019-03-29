ce7148cc8578bd42b2a3423f38f2e7caf2ed0de2
Francisco Duran <Franciscoduran93@gmail.com>
2019-03-29 15:32:41 -0600

    added update bullets and fire bullets

--- game_functions.py
+++ game_functions.py
@@ -10,9 +10,24 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group.
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fire a bullet if limit no reached yet"""
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
+
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets."""
+    # Update bullet positions.
+    bullets.update()
+
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
 
 def check_keyup_events(event, ship):
     """Respond to keypresses"""
@@ -39,7 +54,7 @@ def update_screen(ai_settings, screen, ship, bullets):
     # Redraw all bullets behind ship and aliens.
     for bullet in bullets.sprites():
         bullet.draw_bullet()
-        
+
     ship.blitme()
 
     # Make the most recently drawn screen visible.
